f36e4156a67e5ad91873c68f1db26cc8de600249
Ivan Castillo <total180training@gmail.com>
2021-11-10 05:20:28 -0500

    Passing 4 tests

--- solution.js
+++ solution.js
@@ -1,5 +1,42 @@
 const { nums, words } = require("./data/data.js");
 
+class Node {
+  constructor(data) {
+    this.data = data
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor(head = null) {
+    this.head = head
+  }
+
+  insert(data) {
+    const newNode = new Node(data)
+    newNode.next = this.head
+    this.head = newNode
+    // console.log(newNode)
+  }
+  size() {
+    let count = 0
+    while (this.head) {
+      count++
+      this.head = this.head.next
+    }
+    return count
+  }
+}
+
+const Krystal = new Node("Krystal")
+const Ivan = new Node("Ivan")
+Krystal.next = Ivan
+console.log(Krystal)
+const list = new LinkedList(Krystal)
+list.insert("Erik")
+console.log(list.size())
+
+
 module.exports = {
   Node,
   LinkedList,
