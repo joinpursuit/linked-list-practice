db1da9fdf53c2387e2b5daf7bbba2fdc574f1b02
Jamee Hengari <jhengari@hotmail.com>
2021-11-15 15:01:01 -0500

    linked list lab

--- solution.js
+++ solution.js
@@ -1,5 +1,103 @@
 const { nums, words } = require("./data/data.js");
 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+
+  insert(data) {
+    let newNode = new Node(data);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+
+  size() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
+  search(key) {
+    let node = this.head;
+    while (node !== null && node.data !== key) {
+      node = node.next;
+    }
+    return node;
+  }
+  clear() {
+    this.head = null;
+  }
+  getLast() {
+    let node = this.head;
+    if (!this.head) return null;
+    while (node.next) {
+      node = node.next;
+    }
+    return node;
+  }
+  getFirst() {
+    return this.head;
+  }
+  delete(data) {
+    let node = this.head;
+    let counter = 0;
+    while (node.data !== data && node.next) {
+      counter++;
+      node = node.next;
+    }
+    let foundNode = node;
+    node = this.head;
+    for (let i = 1; i < counter; i++) {
+      node = node.next;
+    }
+    node.next = foundNode.next;
+  }
+  isEmpty() {
+    return !this.head ? true : false;
+  }
+  getKth(k) {
+    let node = this.head;
+    for (let i = 1; i < k; i++) {
+      node = node.next;
+    }
+    return node;
+  }
+  getKthToLast(k) {
+    let lastNode = this.getLast().data + "";
+    return lastNode[k];
+  }
+  toArray() {
+    let arr = [];
+    let node = this.head;
+    while (node) {
+      arr.push(node.data);
+      node = node.next;
+    }
+    return arr;
+  }
+  containsDuplicates() {
+    let arr = [];
+    let node = this.head;
+    while (node) {
+      arr.push(node.data);
+      node = node.next;
+    }
+    return !arr.every((elem, i) => {
+      return arr.indexOf(elem) === i;
+    });
+  }
+}
+
 module.exports = {
   Node,
   LinkedList,
