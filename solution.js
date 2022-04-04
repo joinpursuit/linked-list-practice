5733997da36cc319623393f148c3c6900ce906b1
Milton Clark <kingcoder247@gmail.com>
2022-04-04 13:09:39 -0400

    Create Node class and Linked List. Methods List with no functionality

--- solution.js
+++ solution.js
@@ -1,5 +1,70 @@
 const { nums, words } = require("./data/data.js");
 
+class Node{
+  constructor(data){
+    this.data = data;
+    this.next = null;
+  };
+};
+
+class LinkedList{
+  constructor(head = null){
+    this.head = head;
+  };
+// Methods
+  insert(){
+
+  };
+
+  size(){
+
+  };
+
+  delete(){
+
+  };
+
+  getFirst(){
+
+  };
+
+  getLast(){
+
+  };
+
+  search(){
+    
+  };
+
+  getKth(){
+
+  };
+
+  getKthToLast(){
+
+  };
+
+  isEmpty(){
+
+  };
+
+  clear(){
+
+  };
+
+  toArray(){
+
+  };
+
+  containsDuplicates(){
+    
+  };
+  
+
+
+}
+
+
 module.exports = {
   Node,
   LinkedList,
