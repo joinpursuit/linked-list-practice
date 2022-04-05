62c84ab03a41d5987d51250de8c595a295e48489
salrey <tekdehn@gmail.com>
2022-04-05 15:05:06 -0400

    Create Node and Linked List classes, including insert and size methods

--- solution.js
+++ solution.js
@@ -1,5 +1,58 @@
 const { nums, words } = require("./data/data.js");
 
+class Node {
+  constructor(data, next) {
+    this.data = data || null;
+    this.next = next || null;
+  }
+}
+
+class LinkedList {
+  constructor(head) {
+    this.head = head || null;
+  }
+
+  insert(value) {
+    let newNode = new Node(value)
+    
+    let node = this.head 
+    
+    //insert at the beginning for when we're starting from scratch OR when there's already data
+    if (node == null || node.data > value) {
+      newNode.next = node
+      this.head = newNode
+    }
+    
+    //insert in the middle and end... 
+    while(node) {
+      if (node.data < value && node.next == null) {
+        let temp = node.next
+        node.next = newNode
+        newNode.next = temp
+      }
+      node = node.next
+    }
+
+    return this.head
+
+  }
+
+  size() {
+    let counter = 0;
+
+    let currNode = this.head;
+
+    while (currNode) {
+      counter++
+      currNode = currNode.next
+    }
+
+    return counter
+
+  }
+}
+
+
 module.exports = {
   Node,
   LinkedList,
