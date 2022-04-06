b5ebd71b284b89b9ea6d2a125266810a093990c2
Fatema Kabir <fatema.kabir@pursuit.org>
2022-04-06 11:57:25 -0400

    new node - new linkedList - insert method - count the length - delete by ley - isEmpty method - clear linkedList

--- solution.js
+++ solution.js
@@ -1,5 +1,71 @@
 const { nums, words } = require("./data/data.js");
 
+class Node {
+  constructor(data = null, next = null){
+    this.data = data
+    this.next = next
+  }
+}
+
+class LinkedList {
+  constructor(){
+    this.head = null
+  }
+
+  insert(data){
+    let node = new Node(data)
+    if(this.head === null){
+      this.head = node
+    } else {
+      node.next = this.head
+      this.head = node
+    }
+  }
+
+  size(){
+    let counter = 0
+    let node = this.head
+    while(node !== null){
+      node = node.next
+      ++counter
+    }
+    return counter
+  }
+
+  delete(data){
+    let node = this.head
+    if(node.data === data){
+      this.head = this.head.next
+      return true
+    }
+    while(node !== null){
+      if(node.next.data === data){
+        node.next = node.next.next
+        return true
+      }
+      node = node.next
+    }
+    return false
+  }
+
+  getFirst(){
+    return this.head 
+  }
+
+  isEmpty(){
+    if(this.head === null){
+      return true
+    } else {
+      return false
+    }
+  }
+
+  clear(){
+    this.head = null
+  }
+
+} 
+
 module.exports = {
   Node,
   LinkedList,
