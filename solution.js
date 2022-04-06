3f58683b1de342abdc490b3ecedee9e3224c224e
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2022-04-06 15:00:49 -0400

    created Node & LinkedList classes

--- solution.js
+++ solution.js
@@ -1,5 +1,60 @@
 const { nums, words } = require("./data/data.js");
 
+
+class Node {
+  constructor(data, next){
+    this.data = data;
+    this.next = next;
+  }
+};
+
+class LinkedList{
+  constructor(head = null){
+    this.head = head;
+  }
+  insert(){
+
+  }
+  size(){
+
+  }
+
+  delete(){
+
+  }
+
+  getFirst(){
+
+  }
+
+  getLast(){
+  
+  }
+  search(){
+
+  }
+  getKth(){
+
+  }
+  getKthToLast(){
+
+  }
+  isEmpty(){
+
+  }
+  clear(){
+
+  }
+  toArray(){
+
+  }
+  containsDuplicates(){
+    
+  }
+
+
+}
+
 module.exports = {
   Node,
   LinkedList,
