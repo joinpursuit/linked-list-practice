ca81cc71c2d4554b377a2d41ef3286a1f7474b17
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2022-04-07 15:53:43 -0400

    removed console.logs

--- solution.js
+++ solution.js
@@ -1,18 +1,4 @@
-//const { nums, words } = require("./data/data.js");
-const words = [
-  "the",
-  "quick",
-  "brown",
-  "the",
-  "the",
-  "fox",
-  "jumps",
-  "over",
-  "the",
-  "lazy",
-  "dog",
-];
-const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 0];
+const { nums, words } = require("./data/data.js");
 
 class Node {
   constructor(data, next = null) {
@@ -72,14 +58,12 @@ class LinkedList {
       }
     }
   }
-  //"the", "quick", "brown", "fox", "jumps", "over", "the", "lazy","dog",
   search(key) {
     if (!this.head) return null;
     let temp = this.head;
     let searchedFor = null;
     while (temp) {
       if (temp.data === key) {
-        console.log(`hey this is a searched-key: ${temp.data}`);
         searchedFor = temp;
       }
       temp = temp.next;
@@ -87,36 +71,26 @@ class LinkedList {
     return searchedFor;
   }
   getKth(num) {
-    //how do i keep track of the elements??
-    //need a count
-    //loop through the linkedList
     let temp = this.head;
     let count = 0;
     let kthNode = null;
-    //console.log(this.head);
     while (temp) {
       count++;
       if (count === num) {
-        //console.log(temp.data,"whaddup")
         kthNode = temp;
       }
-      //console.log(temp)
       temp = temp.next;
     }
     return kthNode;
   }
   getKthToLast(num) {
-    //console.log(this.size());
     let kthToLastNum = this.size() - num;
     let kthToLastNode = null;
     let count = 0;
-    //console.log(kthToLastNum);
     let temp = this.head;
-    //console.log(temp)
     while (temp) {
       count++;
       if (kthToLastNum === count) {
-        //console.log(temp,"yo")
         kthToLastNode = temp;
       }
       temp = temp.next;
@@ -131,15 +105,12 @@ class LinkedList {
     }
   }
   clear() {
-    //console.log(this.head);
     this.head.next = null;
     this.head = null;
   }
-  //const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0];
   toArray() {
     let array = [];
     let temp = this.head;
-    //console.log(temp);
     while(temp){
       array.push(temp.data);
       temp = temp.next;
@@ -150,17 +121,11 @@ class LinkedList {
     let temp = this.head;
     let object = {};
     while(temp){
-      console.log(temp.data)
       object[temp.data] = (object[temp.data] || 0) + 1;
       temp = temp.next;
     }
-    console.log(object)
     for(let key in object){
-      console.log(key);
-      console.log(object[key]);
-
       if(object[key] > 1){ 
-        console.log(key, "duplicates")
         return true;
       }
     }
@@ -168,24 +133,6 @@ class LinkedList {
   }
 }
 
-let list = new LinkedList();
-let numList = new LinkedList();
-// console.log(util.inspect(list, true, 10))
-for (let word of words) {
-  list.insert(word);
-}
-
-for (let num of nums) {
-  numList.insert(num);
-}
-// console.log(numList.containsDuplicates());
-console.log(list.containsDuplicates())
-//numList.getKth(1)
-// console.log(list.getLast());
-//list.search("dog");
-//numList.getKthToLast(3);
-//numList.clear()
-//numList.toArray();
 
 module.exports = {
   Node,
