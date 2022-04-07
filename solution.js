2922d15fa0b46cbba36558f82ad750603b7d297f
Christina Loiacono <loiaconochristina@gmail.com>
2022-04-07 13:30:14 -0400

    Fetched upstream because of ERROR in TEST FILE. Now passing 11 tests

--- solution.js
+++ solution.js
@@ -1,3 +1,4 @@
+const util = require("util");
 const { nums, words } = require("./data/data.js");
 
 /* Create a Node class with properties: data, next */
@@ -64,9 +65,7 @@ class LinkedList {
   // This will get the last node of our linked list.
   getLast(){
     let node = this.head;
-    let count = 0;
     while (node){
-      count++;
       node = node.next;
       // if the next node is pointing to null, return the last node.
       if (node.next === null){
@@ -75,16 +74,76 @@ class LinkedList {
     }
   }
 
+  // `search` an element by key and return data
   // This will allow us to search for a matching piece of data.
-  search(){}
-  getKth(){}
+  search(data){
+    let node = this.head;
+    while (node.data !== data && node.next){
+      node = node.next;
+    }
+    return node;
+  }
+
+  // `getKth` retrieve kth element
+  getKth(data){
+    // let node = this.head;
+    // console.log("???Node:", node);
+    // while (node.data !== data && node.next){
+    //   console.log("---this.head", this.head)
+    //   console.log("??node.next:", node.next)
+    //   console.log("???node.data", node.data)
+    //   console.log("whatisdata??:", data)
+    //   node = node.next;
+    // }
+    // return node;
+  }
+
+  // `getKthToLast` retrieve kth from last element
   getKthToLast(){}
-  isEmpty(){}
+
+  // `isEmpty` check if list is empty
+  isEmpty(){
+    let node = this.head;
+    if (node === null){
+      return true;
+    }
+    return false;
+  }
+
   // This will clear our linked list of all the nodes.
-  clear(){}
-  toArray(){}
-  containsDuplicates(){}
+  // moving the pointer of this.head to "nothing"/null
+  clear(){
+    this.head = null;
+  }
+
+  // `toArray` convert data from linked list to an array
+  // Question - where is #10???
+  // 4-7-22 Sam posted on slack: Error in test. Fetch upstream.
+  // After fetch: now passing this test.
+  toArray(data){
+    // let something = data
+    // console.log("something:", something)
+    let node = this.head;
+    // console.log("util:", util.inspect(node, true, 11, true))
+    let newArr = [];
+
+    while (node.next !== null){
+      // console.log("nodeeeee:", node.data)
+      newArr.push(node.data)
+      node = node.next;
+    }
+    newArr.push(node.data);
+    // console.log("??newArr:", newArr)
+    return newArr;
+  }
+
+  // `containsDuplicates` check for duplicates - return true if contains duplicates, false if not
+  containsDuplicates(){
+    // if duplicates - return true
+    // else, return false
+  }
 }
+
 
 // Testing insert method:
 // let newList = new LinkedList();
