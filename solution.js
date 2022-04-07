31f0d234dfc67149d3be222cecacd7f067a698cf
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2022-04-06 20:54:13 -0400

    added getKth, getKthToLast, clear, toArray, and containsduplicates methods

--- solution.js
+++ solution.js
@@ -1,6 +1,6 @@
 const { nums, words } = require("./data/data.js");
 
-
+ 
 class Node {
   constructor(data, next){
     this.data = data;
@@ -11,17 +11,17 @@ class Node {
 class LinkedList{
   constructor(head = null){
     this.head = head;
-  }
+  };
 
   insert(value){
-    let newNode = new Node(value);
+    let nextNode = new Node(value);
     if (!this.head) {
-        this.head = newNode;
+        this.head = nextNode;
     } else {
-        newNode.next = this.head;
-        this.head = newNode;
-    }
-  }
+        nextNode.next = this.head;
+        this.head = nextNode;
+    };
+  };
 
   size(){
     let count = 0;
@@ -31,49 +31,101 @@ class LinkedList{
       temp = temp.next;
     }
     return count;
-  }
+  };
 
-  delete(){
-
-  }
+  delete(key){
+    if(this.head.data === key){
+      this.head = this.head.next;
+    }
+    let nextNode = this.head;
+        while (nextNode.next) {
+            if (nextNode.next.data === key) {
+              nextNode.next = nextNode.next.next;
+          }
+        nextNode = nextNode.next;
+    }
+  };
 
   getFirst(){
       return this.head;
   }
 
   getLast(){
-  
-  }
-  search(value){
-      let temp = this.head
-      while(temp){
-        if(temp.data === value){
-          return temp;
-      }
+    let lastNode = this.head;
+    if (lastNode) {
+        while (lastNode.next) {
+            lastNode = lastNode.next;
+        }
     }
+    return lastNode;
   }
-  getKth(){
 
-  }
-  getKthToLast(){
+  search(key) {
+    if (this.head.data === key) {
+        return this.head;
+    }
 
-  }
+    let nextNode = this.head;
+    while (nextNode.next) {
+        if (nextNode.next.data === key) {
+            return nextNode.next;
+        }
+        nextNode = nextNode.next;
+    }
+    return null;
+}
+  getKth(index){
+    let count = 1;
+    let head = this.head;
+    while (head) {
+        if (index === count) {
+            return head;
+        }
+        count++;
+        head = head.next;
+    }
+    return count;
+  };
+
+  getKthToLast(index){
+    let length = this.size();
+    return this.getKth(length - index);
+  };
+
   isEmpty(){
     if(!this.head){
       return true
-    } else {
+      } else {
       return false;
     }
   };
+
   clear(){
-      this.head = null;
+    this.head = null;
   }
-  toArray(){
 
-  }
-  containsDuplicates(){
+  toArray() {
+    let arr = [];
+    let node = this.head;
+    while (node) {
+      arr.push(node.data);
+      node = node.next;
+    }
+    return arr;
+}
 
-  }
+  containsDuplicates() {
+    let arr = [];
+    let nextNode = this.head;
+      while (nextNode) {
+        if (arr.includes(nextNode.data)) {
+          return true;
+        }
+          arr.push(nextNode.data);
+          nextNode = nextNode.next;
+        }
+        return false;
+    }
 }
 
 module.exports = {
