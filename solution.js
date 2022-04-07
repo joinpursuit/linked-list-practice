275e7a2969bf49391b181a14fdd2ed4b8e5c2a72
andres ballares <andr3sb@hotmail.com>
2022-04-06 23:38:35 -0400

    initialted Node and class

--- solution.js
+++ solution.js
@@ -1,4 +1,27 @@
 const { nums, words } = require("./data/data.js");
+class Node {
+  constructor(data, next){
+    this.data = data;
+    this.next = next || null;
+  }
+}
+
+class LinkedList {
+  constructor(head){
+    this.head = head;
+  }
+
+  insert(data){
+    let newData = new Node(data);
+
+  }
+
+}
+
+//let node1 = new Node(1);
+//let node2 = new Node(2);
+let list = new LinkedList();
+console.log(list)
 
 module.exports = {
   Node,
