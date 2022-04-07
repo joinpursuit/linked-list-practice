979ad2e920ddb6eee3e3d00b72fb6f890181908d
james russell <jamesrussell@pursuit.org>
2022-04-07 12:25:38 -0400

    passing 6 tests

--- solution.js
+++ solution.js
@@ -1,5 +1,72 @@
 const { nums, words } = require("./data/data.js");
 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+  insert(data){
+    let newNode = new Node(data);
+    if(!this.head){
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
+  size(){
+    let count = 0
+    let node = this.head;
+    while(node){
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
+  delete(){
+    if ((this.head === null))
+      throw new Error("Error: The current linked list is empty");
+    
+    let temp = this.head;
+    this.head = this.head.next;
+    return temp.data;
+  }
+  getFirst(){
+    
+  }
+  getLast(){
+    
+  }
+  search(key){
+    let node = this.head;
+    while(node !== null && node.data !== key){
+      node = node.next;
+    }
+    return node;
+  }
+  getKthToLast(){
+
+  }
+  isEmpty(){
+
+  }
+  clear(){
+
+  }
+  toArray(){
+
+  }
+  containsDuplicates(){
+
+  }
+}
+
 module.exports = {
   Node,
   LinkedList,
