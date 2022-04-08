e0281292ada9b03b0b8285251fa3d3f39eb60157
Jenna Qian <jiayin1996@gmail.com>
2022-04-08 09:34:31 -0400

    first commit

--- solution.js
+++ solution.js
@@ -1,4 +1,51 @@
-const { nums, words } = require("./data/data.js");
+// const { nums, words } = require("./data/data.js");
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0];
+
+const words = [
+  "the",
+  "quick",
+  "brown",
+  "fox",
+  "jumps",
+  "over",
+  "the",
+  "lazy",
+  "dog",
+];
+
+class Node {
+  constructor(data, next = null){
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+    this.size = 0;
+  }
+
+  insert(value){
+    let newNode = new Node(value);
+    newNode.next = this.head; 
+    this.head = newNode;
+  }
+}
+
+let newLinkedList1 = new LinkedList();
+
+for(let num of nums){
+  // console.log(num)
+  newLinkedList1.insert(num)
+}
+
+let newLinkedList2 = new LinkedList();
+for(let word of words){
+  newLinkedList2.insert(word)
+}
+
 
 module.exports = {
   Node,
