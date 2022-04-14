ae75b6aa71d63e2cde38357e409aca1fc071fc42
TheodoreBFrazier <theo.frazier@icloud.com>
2022-04-14 14:58:40 -0400

    Constructor complete

--- solution.js
+++ solution.js
@@ -1,5 +1,12 @@
 const { nums, words } = require("./data/data.js");
 
+class Node {
+  constructor(data, next) {
+    this.data = data
+    this.next = next
+  }
+}
+
 module.exports = {
   Node,
   LinkedList,
