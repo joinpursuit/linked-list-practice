48a20188e4645bea721a40cab2c7b2343a9f429d
TheodoreBFrazier <theo.frazier@icloud.com>
2022-04-21 22:15:24 -0400

    10 / 14 test passing

--- solution.js
+++ solution.js
@@ -22,25 +22,105 @@ class LinkedList {
     }
   }
 
-size () {
-  let sizeCount = 0;
-  let node = this.head;
-  while (node) {
-    //increment count
-    sizeCount++
-    nextNode = node.next;
+  size() {
+    let sizeCount = 0;
+    let node = this.head;
+    while (node) {
+      //increment count
+      sizeCount++
+      nextNode = node.next;
+    }
+    return sizeCount
   }
-  return sizeCount
+
+  //delete
+
+  delete(data) {
+    if (this.head.data === data) {
+      this.head = this.head.next;
+    }
+    let nextNode = this.head;
+    while (nextNode.next) {
+      if (nextNode.next.data === data) {
+        nextNode.next = nextNode.next.next;
+      }
+      nextNode = nextNode.next;
+    }
+  };
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    let finalNode = this.head;
+    if (finalNode) {
+      while (finalNode.next) {
+        finalNode = finalNode.next
+      }
+    }
+    return finalNode
+  }
+
+
+  search(key) {
+    let node = this.head;
+    while (node !== null && node.data !== key) {
+      node = node.next;
+    }
+    return node;
+  }
+
+  getKth(arrayIndex) {
+    let arrayCount = 1
+    let head = this.head
+
+    while (head) {
+      if (arrayIndex === arrayCount) {
+        return head;
+      }
+      arrayCount++
+      head = head.next;
+    }
+    return arrayCount
+
+  }
+
+  getKthToLast() {
+
+  }
+  isEmpty() {
+    if (!this.head) {
+      return true;
+    }
+    return false;
+  }
+  clear() {
+    this.head = null;
+  }
+
+  containsDuplicates() {
+
+    let arr = [];
+    let nextNode = this.head;
+      while (nextNode) {
+        if (arr.includes(nextNode.data)) {
+          return true;
+        }
+          arr.push(nextNode.data);
+          nextNode = nextNode.next;
+        }
+        return false;
+    }
 }
 
 
-}
+
+
 
 //getting first element 
 
-getFirst() {
-  return this.head
-}
+
 
 
 
