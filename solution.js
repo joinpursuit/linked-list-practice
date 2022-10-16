f78a942a82e3e760db7db1147ed693cbea29faf0
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-10-16 16:39:02 -0400

    containsDuplicates

--- solution.js
+++ solution.js
@@ -1,38 +1,105 @@
 const { nums, words } = require("./data/data.js");
 
 class Node {
-  constructor(data,next){
+  constructor(data) {
     this.data = data;
-    this.next = next;
+    this.next = null;
   }
 }
-class LinkedList{
-  constructor(head = null){
+class LinkedList {
+  constructor(head = null) {
     this.head = head;
   }
 
-  insert(data){
-    let newNode = new Node(data)
-    if(!this.head){
-      this.head = newNode
-    }else{
-      newNode.next = this.head
-      this.head = new Node
+  insert(data) {
+    let newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
     }
   }
 
-  size(){
+  size() {
     let count = 0;
     let node = this.head;
-    while(node){
+    while (node) {
       count++;
-      node = node.next 
+      node = node.next;
     }
-    return count
+    return count;
   }
 
-}
+  delete(data) {
+    let node = this.head;
+    let counter = 0;
+    while (node.data !== data && node.next) {
+      counter++;
+      node = node.next;
+    }
+    let foundNode = node;
+    node = this.head;
+    for (let i = 1; i < counter; i++) {
+      node = node.next;
+    }
+    node.next = foundNode.next;
+  }
 
+  getFirst() {
+    return this.head;
+  }
+  getLast() {
+    let node = this.head;
+    if (!this.head) {
+      return null;
+    }
+    while (node.next) {
+      node = node.next;
+    }
+
+    return node;
+  }
+
+  search(key) {
+    let node = this.head;
+    while (node !== null && node.data !== key) {
+      node = node.next;
+    }
+    return node;
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  isEmpty() {
+    return !this.head;
+  }
+
+  toArray() {
+    let array = [];
+    let node = this.head;
+    while (node) {
+      array.push(node.data);
+      node = node.next;
+    }
+    return array;
+  }
+
+  containsDuplicates() {
+    let node = this.head;
+    let array = [];
+    while (node) {
+      if (array.includes(node.data)) {
+        array.push(node.data);
+        node = node.next;
+        return true;
+      }
+    }
+    return false;
+  }
+}
 
 module.exports = {
   Node,
