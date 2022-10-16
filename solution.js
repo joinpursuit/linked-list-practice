e125bc4b26f9f9e093c7ad4ac7d227eafffe5a61
Samantha Figueroa <96448061+samfigueroa92@users.noreply.github.com>
2022-10-16 14:29:14 -0400

    creates node and linkedlist class

--- solution.js
+++ solution.js
@@ -1,5 +1,41 @@
 const { nums, words } = require("./data/data.js");
 
+class Node {
+  constructor(data){
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(head = null){
+    this.head = head;
+  }
+
+  insert(data){
+    let newNode = new Node(data);
+    if(!this.head){
+      this.head = newNode;
+    }else{
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
+
+  size(){
+
+  }
+
+  delete(){
+
+  }
+
+  getFirst(){
+    
+  }
+
+}
+
 module.exports = {
   Node,
   LinkedList,
