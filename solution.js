839eefd290f88b4da3ec8387e66943e88c9aad7f
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-10-16 14:55:23 -0400

    insert

--- solution.js
+++ solution.js
@@ -1,5 +1,39 @@
 const { nums, words } = require("./data/data.js");
 
+class Node {
+  constructor(data,next){
+    this.data = data;
+    this.next = next;
+  }
+}
+class LinkedList{
+  constructor(head = null){
+    this.head = head;
+  }
+
+  insert(data){
+    let newNode = new Node(data)
+    if(!this.head){
+      this.head = newNode
+    }else{
+      newNode.next = this.head
+      this.head = new Node
+    }
+  }
+
+  size(){
+    let count = 0;
+    let node = this.head;
+    while(node){
+      count++;
+      node = node.next 
+    }
+    return count
+  }
+
+}
+
+
 module.exports = {
   Node,
   LinkedList,
