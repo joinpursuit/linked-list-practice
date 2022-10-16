ca9e8b7f056b32c8b54bc560a24c05a718c1ac25
EmaleeSoto <emaleesoto@gmail.com>
2022-10-16 15:59:04 -0400

    Created Classes

--- solution.js
+++ solution.js
@@ -1,5 +1,18 @@
 const { nums, words } = require("./data/data.js");
 
+class Node {
+  constructor(data, next) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor(head) {
+    this.head = head;
+  }
+}
+
 module.exports = {
   Node,
   LinkedList,
