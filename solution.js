f8ff594616dba8eeb1e0d5d035ee614699a797b6
named.josie <named.josie@gmail.com>
2022-10-16 23:54:32 -0400

    get first, getlast and insert

--- solution.js
+++ solution.js
@@ -10,6 +10,26 @@ class LinkedList {
   constructor(head) {
     this.head = head;
   }
+  getFirst() {
+    return this.head;
+  }
+  getLast() {
+    if (!this.head) return null;
+    let node = this.head;
+    while (node.next) {
+      node = node.next;
+    }
+    return node;
+  }
+  insert(data) {
+    let newInsertNode = new Node(data);
+    if (!this.head) {
+      this.head = newInsertNode;
+    } else {
+      newInsertNode.next = this.head;
+      this.head = newInsertNode;
+    }
+  }
 }
 
 module.exports = {
