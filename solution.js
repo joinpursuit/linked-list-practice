6c1a9b50ba424a4fd81c0c963a9fa0bc00b86668
H.C.Guerrero <clydeguerrero1@gmail.com>
2022-10-24 00:36:51 -0400

    8 tests passed

--- solution.js
+++ solution.js
@@ -1,4 +1,73 @@
-const { nums, words } = require("./data/data.js");
+const { nums, words } = require('./data/data.js');
+class Node {
+  constructor(data, next) {
+    this.data = data;
+    this.next = next;
+  }
+}
+class LinkedList {
+  insert(value) {
+    let nodeToInsert = new Node(value);
+
+    if (!this.head) {
+      this.head = nodeToInsert;
+      return;
+    } else {
+      nodeToInsert.next = this.head;
+      this.head = nodeToInsert;
+    }
+  }
+  getFirstAndRemove() {
+    let oldHead = this.head;
+    this.head = this.head.next;
+
+    return oldHead;
+  }
+  size() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
+  delete(data) {
+    let node = this.head;
+    let counter = 0;
+    while (node.data !== data && node.next) {
+      counter++;
+      node = node.next;
+    }
+    let selectNode = node;
+    node = this.head;
+    for (let i = 1; i < counter; i++) {
+      node = node.next;
+    }
+
+    node.next = selectNode.next;
+  }
+  getFirst() {
+    return this.head;
+  }
+  getLast() {
+    let node = this.head;
+    if (!this.head) {
+      return null;
+    }
+    while (node.next) {
+      node = node.next;
+    }
+    return node;
+  }
+  search(key) {
+    let node = this.head;
+    while (node !== null && node.data !== key) {
+      node = node.next;
+    }
+    return node;
+  }
+}
 
 module.exports = {
   Node,
