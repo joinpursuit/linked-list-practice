5f6858ab6f8fa8e406224152d186735b9f1f4827
Andres Varon <anvaron@gmail.com>
2022-10-28 14:21:52 -0400

    Tests: new node, new linked list - tested

--- solution.js
+++ solution.js
@@ -1,7 +1,7 @@
 const { nums, words } = require("./data/data.js");
 
 // Tests:
-// Can create a new node
+// Can create a new node [ok]
 class Node {
   constructor(data, next) {
     this.data = data;
@@ -9,18 +9,37 @@ class Node {
   }
 }
 
-// Can create a new linked list
+// Can create a new linked list [ok]
 class LinkedList {
   constructor(head) {
     this.head = head;
   }
 
-  insert() {
-    
+  // Add to the linked list using an insert method
+  insert(data) {
+    // Creating a new node
+    let newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
   }
 
  
 }
+
+const firstNode = new Node(1);
+const secondNode = new Node(2);
+firstNode.next = secondNode;
+
+console.log(firstNode);
+
+const firstList = new LinkedList(firstNode);
+console.log(firstList);
+
+
 
 module.exports = {
   Node,
