8cfeb672df2eda500b26b8f7c34900e6fb993844
Andres Varon <anvaron@gmail.com>
2022-10-28 14:06:46 -0400

    initial commit

--- solution.js
+++ solution.js
@@ -1,5 +1,27 @@
 const { nums, words } = require("./data/data.js");
 
+// Tests:
+// Can create a new node
+class Node {
+  constructor(data, next) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+// Can create a new linked list
+class LinkedList {
+  constructor(head) {
+    this.head = head;
+  }
+
+  insert() {
+    
+  }
+
+ 
+}
+
 module.exports = {
   Node,
   LinkedList,
